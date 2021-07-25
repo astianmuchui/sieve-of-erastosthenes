bfcd36ada7aea8dd01c2e2e1681a37b82acc94ef
seb <Sebastianmuchui79@gmail.com>
2021-07-25 17:58:26 +0300

    Elimination. Still not done yet

---
+++ main.py
@@ -0,0 +1,16 @@
+#Sieve of erasthosthenes
+numbers = ['2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21']
+for x in numbers:
+    y=int(x)
+    z = str(x)
+    if y%2 ==0:
+        #print(y)
+        
+        if y%3 == 0:
+            round(y)
+            if y%5 == 0:
+                round(y)
+            else:
+                print(y)    
+        else:
+            round(y)
